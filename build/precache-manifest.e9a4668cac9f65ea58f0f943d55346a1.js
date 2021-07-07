9ba58d5383b22b6a191873b6794c19a690d98afc
Karan Kangude <karankangude17@gmail.com>
2021-07-07 15:24:58 +0530

    build changes

---
+++ build/precache-manifest.e9a4668cac9f65ea58f0f943d55346a1.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01a3b2e15c7f64f56887cdc9e6e444c",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/index.html"
+  },
+  {
+    "revision": "924b1a1c88fbedd97a3a",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/css/9.5665f477.chunk.css"
+  },
+  {
+    "revision": "6c7440de6eb437761542",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/css/main.5d3f4372.chunk.css"
+  },
+  {
+    "revision": "d7c698a15f0fd12a5d49",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/0.973f2d8b.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/0.973f2d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452ae740343a2d6d7880",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/1.5e59f126.chunk.js"
+  },
+  {
+    "revision": "d5014d9f0e453b8b5903",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/10.a9fb0a0a.chunk.js"
+  },
+  {
+    "revision": "a3d009d38e8e0eeed5ec2d423ae4e079",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/10.a9fb0a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92fcbda43f49c049c59f",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/11.25e2b7cd.chunk.js"
+  },
+  {
+    "revision": "3a97e33f85f209386bc54b7375408b8c",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/11.25e2b7cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fb1bc7c2ba06448a6c",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/12.f56e4649.chunk.js"
+  },
+  {
+    "revision": "23d6d2ddaccdbc97de73",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/13.0b537148.chunk.js"
+  },
+  {
+    "revision": "7f04293a403f2116d365",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/14.a293cc46.chunk.js"
+  },
+  {
+    "revision": "c03a059852fd9492444d",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/15.a87aaf64.chunk.js"
+  },
+  {
+    "revision": "8ad2cf6ce2e97f0e1559",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/16.a891b233.chunk.js"
+  },
+  {
+    "revision": "9946aa023cb086dc5169",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/17.6bb10a2f.chunk.js"
+  },
+  {
+    "revision": "0642d22ff6c1a99e5014",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/18.b7067438.chunk.js"
+  },
+  {
+    "revision": "55337c980cbc0ca556d6",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/19.52bf392d.chunk.js"
+  },
+  {
+    "revision": "a77b9e32078abf9db160",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/2.b4b55823.chunk.js"
+  },
+  {
+    "revision": "7c54332f4fd239e98111",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/20.3c08c622.chunk.js"
+  },
+  {
+    "revision": "a368360403054f00d15c",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/21.acba0593.chunk.js"
+  },
+  {
+    "revision": "7db5204a94dadc84f027",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/22.72636622.chunk.js"
+  },
+  {
+    "revision": "dbaa607b030dcfb69ccd",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/23.d12a2565.chunk.js"
+  },
+  {
+    "revision": "df177799fd7519553493",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/24.7348d986.chunk.js"
+  },
+  {
+    "revision": "6c5ebbe71971e91de34d",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/25.b5f3b530.chunk.js"
+  },
+  {
+    "revision": "9e4fb5e5f60408b70e23",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/26.80cf5dbe.chunk.js"
+  },
+  {
+    "revision": "9da23c4bbd8ea05afad0",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/27.07707bc2.chunk.js"
+  },
+  {
+    "revision": "cac67e812f5ad5d63448",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/28.56c33113.chunk.js"
+  },
+  {
+    "revision": "ecb00d99dd322676ce17",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/29.16e1d5ec.chunk.js"
+  },
+  {
+    "revision": "b2301526771cd957a77b",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/3.e0357c83.chunk.js"
+  },
+  {
+    "revision": "2428ef1b3d35a62f4c0d",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/30.7841b21e.chunk.js"
+  },
+  {
+    "revision": "e9b114ff21d9f33896dd",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/31.c2ff48d0.chunk.js"
+  },
+  {
+    "revision": "601c83c85129f3955a169de052f2ac58",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/31.c2ff48d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e35d16398b4ab7bc1ec",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/4.ddfe8da3.chunk.js"
+  },
+  {
+    "revision": "cdddf20c8a1d5abfac7c",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/5.2a9bc49f.chunk.js"
+  },
+  {
+    "revision": "9bbf78789f54279ba227",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/6.aeeef9c4.chunk.js"
+  },
+  {
+    "revision": "924b1a1c88fbedd97a3a",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/9.c41f0c83.chunk.js"
+  },
+  {
+    "revision": "a7f000e6c23bb9f9a9c3cb99308fa3cb",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/9.c41f0c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7440de6eb437761542",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/main.b85acc9d.chunk.js"
+  },
+  {
+    "revision": "5ed6f7d727c9ffad7a10",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/js/runtime-main.d9c43220.js"
+  },
+  {
+    "revision": "a4b7314a7f1a4c7edda72acdf7652ea3",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/circle-cropped.a4b7314a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8539ea866722b2211c69afe4b7adc227",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/logo-mini.8539ea86.svg"
+  },
+  {
+    "revision": "043774d1de6da96b11c8ade1a61c60cb",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/materialdesignicons-webfont.043774d1.woff"
+  },
+  {
+    "revision": "27cb2cf143cf204f7cfc210089fd10e8",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/materialdesignicons-webfont.27cb2cf1.woff2"
+  },
+  {
+    "revision": "9bfeb985a91e5545d78b906676d8e6fb",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/materialdesignicons-webfont.9bfeb985.ttf"
+  },
+  {
+    "revision": "e971abae304374a81d9f7cea55039de8",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/materialdesignicons-webfont.e971abae.eot"
+  },
+  {
+    "revision": "d7a38674907db85ce866f47b012b147f",
+    "url": "/demo/star-admin-free/react/template/demo_1/preview/static/media/token.d7a38674.png"
+  }
+]);
